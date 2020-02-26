47635e4abf10946ff53b9780aa06858bfc29a2d2
Will Lorand <william_lorand@intuit.com>
2020-02-26 15:20:38 -0800

    added py automate script for delete flow

--- iam_mydata_delete_flow_auto.py
+++ iam_mydata_delete_flow_auto.py
@@ -1,10 +1,10 @@
-# FILE: iam_mydata_download_flow_auto.py
-# DESC: use Selenium with Python to automate Download work order creation on E2E
+# FILE: iam_mydata_delete_flow_auto.py
+# DESC: use Selenium with Python to automate Delete work order creation on E2E
 from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -40,7 +40,7 @@ def wait_for_elem_select(selector):
 #                                SCRIPT LOGIC
 # ------------------------------------------------------------------------------
 
-# Tests Auth Download Flow (No Products) on E2E
+# Tests Auth Delete Flow (No Products) on E2E
 browser.get(IAM_AUTH_URL_E2E)
 browser.maximize_window()
 
@@ -49,19 +49,23 @@ wait_for_elem_select('#ius-userid').send_keys(TEST_USERNAME)
 wait_for_elem_select('#ius-password').send_keys(TEST_USERPASS)
 wait_for_elem_select('button[name="SignIn"]').click()
 
-# 2: Click Data & Privacy, Download 
-WebDriverWait(browser, WAIT_TIMEOUT).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-automation="downloadManager-Continue-button"]'))).click()
+# 2: Click Data & Privacy, Delete
+WebDriverWait(browser, WAIT_TIMEOUT).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-automation="deleteManager-Continue-button"]'))).click()
 
-# 3: Move thru the Download Flow Pages (No Products)
+# 3: Move thru the Delete Flow Pages (No Products)
 # 3.1: Primer Page
 wait_for_elem_select('button[data-automation="continue-button"]').click()
 
-# 3.2: Start Page
+# 3.2: Start Page 
+wait_for_elem_select('button[class*="idsButton--primary"]').click()
+
+# 3.3: Delete Acknowledgement Page
+wait_for_elem_select('input[class="idsCheckbox__input"]').click()
 wait_for_elem_select('button[data-automation="continue-button"]').click()
 
-# 3.3: Download Confirm Page (No Products)
+# 3.4: Delete Confirm Page (No Products)
 wait_for_elem_select('input[data-automation="password-field"]').send_keys(TEST_USERPASS)
-wait_for_elem_select('button[data-automation="continue-button"]').click()
+wait_for_elem_select('button[data-automation="delete-button"]').click()
 
-# 3.4: Success Page
+# 3.5: Success Page
 wait_for_elem_select('button[data-automation="done-button"]').click()
